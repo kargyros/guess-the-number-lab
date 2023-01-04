218110cfbab4ebbdf0ae0caef7231eb9dd618c17
Kostas Argyros <kostasrgrs@gmail.com>
2023-01-04 14:59:59 -0500

    Core Memory Game

--- app.js
+++ app.js
@@ -8,3 +8,33 @@ const game = {
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
   }
 }
+
+
+
+
+
+/*--------------------TASKS----------------------
+
+
+
+
+
+
+
+
+
+/**-----------------FEATURES--------------------
+Allow the player to continually be prompted to enter their guess at the secret number until they guess correctly.
+
+If the player has an incorrect guess, display an alert message that informs the player:
+
+- Whether their guess is too high or too low, and…
+
+- A list of all the previously guessed numbers (without showing the square brackets of an array). 
+
+- If the player has guessed the secret number:
+
+- Display an alert message that congrats the player and informs them of how many guesses they took.
+
+- End the gameplay.
+*/
